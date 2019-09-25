638e005e3ed5ad8ef9379bb2b3e8d89895d76a68
Tharushi De Silva <desilva.watp@gmail.com>
2019-09-26 00:12:07 +0530

    login and reg
    move between screens

---
+++ it16051980/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  it16051980
+//
+//  Copyright © 2019 Tharushi De Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // Show the registration page
+    @IBAction func onRegBtnClick(_ sender: Any) {
+        performSegue(withIdentifier: "showRegistration", sender: self)
+    }
+    
+    // Show the login page
+    @IBAction func onLoginBtnClick(_ sender: Any) {
+        performSegue(withIdentifier: "showLogin", sender: self)
+    }
+    
+    // Login button in login page is clicked.
+    @IBAction func onCorrectLoginBtnClick(_ sender: Any) {
+        print("logging in...")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
